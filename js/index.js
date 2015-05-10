a518804667bba619ab2b44ca4f4438b06dadb344
canghaiyifeng <canghaiyifeng@126.com>
2015-05-10 21:51:00 +0800

    tt

--- js/index.js
+++ js/index.js
@@ -1,7 +1,22 @@
+$(document).ready(function() {
 
-$(document).ready(function(){
-	
-    
-});//end document ready
+	var target = $('#picScroll');
+	var left = 0;
+	var speed = 30;
 
-
+	function Marqeen() {
+		if (target[0].offsetWidth <= left) {
+			left -= target[0].offsetWidth;
+		} else {
+			left++;
+		}
+		target.scrollLeft(left);
+	}
+	var marQueen = window.setInterval(Marqeen, speed);
+	target.mouseover(function() {
+		clearInterval(marQueen);
+	});
+	target.mouseout(function() {
+		marQueen = window.setInterval(Marqeen, speed);
+	});
+}); //end document ready
